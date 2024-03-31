fabe296647c77a87edd6851b9d33d7e88d05ebe3
akar0903 <av0019@srmist.edu.in>
2024-03-31 17:40:54 +0530

    [Arun]:Forget Password UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'forgett',component:ForgetPasswordComponent},
+  {path:'log',component:LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForgetPasswordComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
